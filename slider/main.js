8fe72597544d5a4d2f99b3699322f7469d81b884
AKmahim <mdmahimhossen134@gmail.com>
2022-04-18 04:35:55 +0600

    Slider

---
+++ slider/main.js
@@ -0,0 +1,18 @@
+// ====== slider js ==========
+
+// this is the slider function that we can get from https://kenwheeler.github.io/slick/ 
+// we can style the slider as our needed
+
+
+$('.slider-item').slick({
+    // dots: true,
+    arrows:false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    // centerMode: true,
+    // variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+
+  });
---
+++ slider/readme.md
@@ -0,0 +1,4 @@
+# slider example using slick slider
+## Demo link ---- >> https://akmahim.github.io/Front-end-components-build-up/slider/
+
+![Slider](https://github.com/AKmahim/Front-end-components-build-up/blob/master/slider/slider.png)
